6572fa2dd06d4c1d1136c8fd96c325f164699146
Tnmgdrelifousaem <99417161+Tnmgdrelifousaem@users.noreply.github.com>
2024-03-16 02:07:43 +0300

    Create Geometric.java

---
+++ 1.Geometric Classes/Geometric.java
@@ -0,0 +1,64 @@
+// Базовый класс для геометрических фигур
+//Verigin 
+abstract class Shape {
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+}
+
+// Класс для круга
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Square extends Rectangle {
+    private double side;
+
+    public Square(double side) {
+        super(side, side);
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 4 * side;
+    }
+}
